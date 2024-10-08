fcf22ba004f50ab1246840642e7049b012a03b22
igprad <ign.aldi.pradana@gmail.com>
2024-10-08 19:32:16 +0700

    chore: refactor to split the implementation to separated files (#4)

---
+++ src/commands/util/parse-args.ts
@@ -0,0 +1,19 @@
+type Args = {
+    url: string;
+};
+
+function parseArgs(message: string): Args | undefined {
+    const args = message.split(" ");
+    if (args.length < 2) return;
+
+    const url = args[1];
+
+    return {
+        url
+    };
+}
+
+export {
+    Args,
+    parseArgs
+}
