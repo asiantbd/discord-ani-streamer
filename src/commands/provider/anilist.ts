becdd7ba34fd69ce47783cdd50bd33eb74dd1531
heronimus <adie@heronimus.id>
2024-10-12 21:26:59 +0700

    feat: add allanime gql provider

--- src/commands/provider/anilist.ts
+++ src/commands/provider/anilist.ts
@@ -2,41 +2,43 @@ import fetch from "node-fetch";
 
 const graphqlAnilistEndpoint = "https://graphql.anilist.co/";
 
-export async function querySearchAnime(title: string): Promise<any> {
-  let schema = `query Query($id: Int, $page: Int, $perPage: Int, $search: String, $sort: [MediaSort], $type: MediaType, $statusNotIn: [MediaStatus]) {
-      Page (page: $page, perPage: $perPage) {
-        pageInfo {
-          currentPage
-          hasNextPage
-          perPage
-        }
-        media(id: $id, search: $search, sort: $sort, type: $type, status_not_in: $statusNotIn) {
-          title {
-            english
-            native
-            romaji
-          }
-          episodes
-          meanScore
-          airingSchedule {
-            edges {
-              node {
-                airingAt
-                episode
-                timeUntilAiring
+export async function querySearchAnilist(title: string): Promise<any> {
+  let schema = `query Query($id: Int, $page: Int, $perPage: Int, $search: String, $sort: [MediaSort], $type: MediaType, $statusNotIn: [MediaStatus], $startDateGreater: FuzzyDateInt, $averageScoreGreater: Int) {
+          Page (page: $page, perPage: $perPage) {
+            pageInfo {
+              currentPage
+              hasNextPage
+              perPage
+            }
+            media(id: $id, search: $search, sort: $sort, type: $type, status_not_in: $statusNotIn, startDate_greater: $startDateGreater, averageScore_greater: $averageScoreGreater) {
+              title {
+                english
+                native
+                romaji
               }
+              episodes
+              meanScore
+              airingSchedule {
+                edges {
+                  node {
+                    airingAt
+                    episode
+                    timeUntilAiring
+                  }
+                }
+              }
+              coverImage {
+                medium
+                extraLarge
+                large
+              }
+              season
+              seasonYear
+              id
+              averageScore
             }
           }
-          coverImage {
-            medium
-            extraLarge
-            large
-          }
-          season
-          seasonYear
-        }
-      }
-    }`;
+        }`;
 
   let variable = {
     search: title,
@@ -45,6 +47,7 @@ export async function querySearchAnime(title: string): Promise<any> {
     sort: "START_DATE_DESC",
     type: "ANIME",
     statusNotIn: ["NOT_YET_RELEASED", "CANCELLED"],
+    averageScoreGreater: 45,
   };
   let response = await fetch(graphqlAnilistEndpoint, {
     method: "POST",
